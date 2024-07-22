48aac16208556d802d6677effe5013a7834e3bec
James Green <james.green@freeagent.com>
2024-07-22 15:21:20 +0100

    initial commit

---
+++ lib/rulers.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "rulers/version"
+
+module Rulers
+  class Error < StandardError; end
+
+  class Application
+    def call(env)
+      puts env.inspect
+      [200, {'Content-Type' => 'text/html'},
+      ["Hello from Ruby on Rulers!"]]
+    end
+  end  
+end
