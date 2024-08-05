b425587f4559de65f6c9e4292c95af2165d0c06f
James Green <james.green@freeagent.com>
2024-08-05 12:44:00 +0100

    add part 1 exercises

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require "rulers/array"
 
 module Rulers
   class Error < StandardError; end
 
   class Application
     def call(env)
-      puts env.inspect
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
       ["Hello from Ruby on Rulers!"]]
     end
